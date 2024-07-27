07cec0393035451a44fc6beb39208c33d44c49a0
DHOT Networks - Evolving Technologies... <85343506+DHOTNetworks@users.noreply.github.com>
2024-07-28 00:03:25 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -21,11 +21,11 @@ export const About = () => {
                 Company
               </h2>
               <p className="text-xl text-muted-foreground mt-4 text-justify">
-                Zephyria Network aims to revolutionize blockchain with scalable,
-                secure, and EVM-compatible solutions. We use advanced technology
+                Zephyria Network aims to revolutionize the Blockchain Tech with Ultra Scalable,
+                Secure and EVM-compatible Succinct solution. We look forward to revolutionize technology
                 to tackle current challenges, ensuring seamless integration and
                 robust performance. Join us to shape the future of decentralized
-                tech and drive mass adoption.
+                tech and drive mass adoption of web3.
               </p>
             </div>
 
